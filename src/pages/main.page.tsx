840486a8065394277407ff242ec13002fbb459d2
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-08-18 17:57:24 +0200

    update main page with facts and menu

--- src/pages/main.page.tsx
+++ src/pages/main.page.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Header, Main, Footer, MainTitle, ButtonsContainer } from 'containers';
+import { Header, Main, Footer, MainTitle, ButtonsContainer, FactsContainer, MenuContainer } from 'containers';
 
 export const MainPage: React.FC = () => {
   return (
@@ -8,8 +8,10 @@ export const MainPage: React.FC = () => {
       <Main>
         <MainTitle>Chuck Norris Facts</MainTitle>
         <ButtonsContainer />
+        <FactsContainer />
       </Main>
       <Footer />
+      <MenuContainer />
     </React.Fragment>
   );
 };
