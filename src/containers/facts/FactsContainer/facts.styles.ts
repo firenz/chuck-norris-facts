4f55a932e8ff9420368c4fb125e6b9152894c7c5
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-08-18 17:46:46 +0200

    add facts container

---
+++ src/containers/facts/FactsContainer/facts.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const FactsLayout = styled.div`
+  justify-self: center;
+  flex-grow: 1;
+
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: center;
+  justify-content: flex-end;
+
+  max-width: 40rem;
+  width: 95%;
+  padding-bottom: 1rem;
+`;
