8baff3a90ce64a8d998ec1ce254939ef69304ad0
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-08-18 17:45:53 +0200

    add fact list container

---
+++ src/containers/facts/FactsContainer/FactList/fact-list.container.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { FactListLayout } from './fact-list.styles';
+import { FactElement } from '../FactElement';
+
+interface Props {
+  facts: string[];
+}
+
+export const FactsList: React.FC<Props> = (props: Props) => {
+  const { facts } = props;
+
+  return (
+    <FactListLayout>
+      {facts.map((fact: string, index: number) => (
+        <FactElement key={index} fact={fact} />
+      ))}
+    </FactListLayout>
+  );
+};
---
+++ src/containers/facts/FactsContainer/FactList/fact-list.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const FactListLayout = styled.ul`
+  list-style: none;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-start: 0;
+  margin-inline-end: 0;
+  padding-inline-start: 0;
+
+  margin: 0;
+`;
