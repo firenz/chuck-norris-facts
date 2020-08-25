6ccbc62635c4ff4c558d89022177c851cbb5be71
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-08-26 00:51:07 +0200

    fix random number generator util

--- src/common/utils/random-number.utils.ts
+++ src/common/utils/random-number.utils.ts
@@ -1,7 +1,7 @@
 export const generateRandomNumber = (length: number): number => {
   if (length <= 0) return NaN;
 
-  const minValue = 1 * Math.pow(10, length);
-  const maxValue = 9 * Math.pow(10, length);
+  const minValue = 1 * Math.pow(10, length - 1);
+  const maxValue = 9 * Math.pow(10, length - 1);
   return Math.floor(minValue + Math.random() * maxValue);
 };
