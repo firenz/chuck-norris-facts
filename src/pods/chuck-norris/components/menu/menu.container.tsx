a49d754147dc921ac133c6f505c1015a68b066bc
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-08-26 01:01:11 +0200

    update menu container

--- src/pods/chuck-norris/components/menu/menu.container.tsx
+++ src/pods/chuck-norris/components/menu/menu.container.tsx
@@ -1,29 +1,35 @@
 import * as React from 'react';
 import { useQuery } from 'react-query';
-import { SessionContext } from 'core';
+import { SessionContext } from 'pods/chuck-norris/chuck-norris.context';
 
-import { getCategoriesApi } from 'pods/chuck-norris/api';
+import { fetchCategoriesApi } from 'pods/chuck-norris/api';
 import { mapperCategoriesApiToVm } from 'pods/chuck-norris/chuck-norris.mapper';
-import { MenuWrapper, MenuLayout } from './menu.styles';
-import { RandomButton } from './random-button';
-import { MenuTitle } from './menu-title';
+import { MenuLayout, MenuBackground, MenuBottomLayout } from './menu.styles';
 import { CategoriesContainer } from './categories';
+import { MenuClearFacts } from './clear-facts';
+import { Cancel } from './cancel';
+import { CancelIcon } from './cancel-icon';
 
 export const MenuContainer: React.FC = () => {
   const { showMenu } = React.useContext(SessionContext);
   const { data } = useQuery('getCategories', async () => {
-    return mapperCategoriesApiToVm(await getCategoriesApi());
+    return mapperCategoriesApiToVm(await fetchCategoriesApi());
   });
 
   if (!showMenu) return null;
 
   return (
-    <MenuWrapper>
+    <>
+      <MenuBackground aria-hidden="true" />
       <MenuLayout>
-        <RandomButton />
-        <MenuTitle>Categories</MenuTitle>
+        <MenuClearFacts />
+        <CancelIcon />
+        <h1>choose a category</h1>
         <CategoriesContainer categories={data || []} />
+        <MenuBottomLayout>
+          <Cancel />
+        </MenuBottomLayout>
       </MenuLayout>
-    </MenuWrapper>
+    </>
   );
 };
