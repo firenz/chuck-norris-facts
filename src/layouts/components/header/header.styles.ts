668ee846c9ed4544f4a1de78f5e125165a693aa1
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-08-26 00:52:18 +0200

    update header layout

--- src/layouts/components/header/header.styles.ts
+++ src/layouts/components/header/header.styles.ts
@@ -1,13 +1,24 @@
 import styled from 'styled-components';
 
 export const HeaderLayout = styled.header`
-  @media (min-width: 800px) {
+  @media (min-width: 980px) {
+    position: fixed;
+    top: 4rem;
+    left: 16%;
+    z-index: 15;
+
+    margin-left: -3rem;
   }
 `;
 
 export const Character = styled.img`
   height: 20rem;
   margin-top: 1rem;
+
+  @media (min-width: 980px) {
+    width: 401px;
+    height: 376.45px;
+  }
 `;
 
 export const Logo = styled.img`
@@ -15,4 +26,8 @@ export const Logo = styled.img`
   top: 12rem;
   left: 50%;
   transform: translate(-50%);
+
+  @media (min-width: 980px) {
+    margin-top: 6%;
+  }
 `;
