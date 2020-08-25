bc8d047396779f9b93f8022690f1f04f6957056a
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-08-26 00:53:03 +0200

    add background to chuck norris pod

--- src/pods/chuck-norris/chuck-norris.component.tsx
+++ src/pods/chuck-norris/chuck-norris.component.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 
 import { ButtonsContainer, FactsContainer, MenuContainer } from './components';
+import { BackgroundImage } from './chuck-norris.styles';
+import { mountainBackground } from 'assets/svg';
 
 export const ChuckNorrisComponent: React.FC = () => {
   return (
     <>
+      <BackgroundImage
+        src={mountainBackground}
+        alt="mountain background"
+        aria-hidden="true"
+      />
       <ButtonsContainer />
       <MenuContainer />
       <FactsContainer />
