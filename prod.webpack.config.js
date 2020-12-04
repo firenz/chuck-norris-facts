a366f62711f681218bc46cf700cc4d7a1eb5b0bc
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-12-04 15:07:28 +0100

    update webpack prod config file

--- prod.webpack.config.js
+++ prod.webpack.config.js
@@ -5,7 +5,7 @@ const base = require('./base.webpack.config');
 module.exports = merge(base, {
   mode: 'production',
   output: {
-    publicPath: '/codeko-jobtest/',
+    publicPath: '/chuck-norris-facts/',
     filename: '[name].[chunkhash].js',
   },
   module: {
